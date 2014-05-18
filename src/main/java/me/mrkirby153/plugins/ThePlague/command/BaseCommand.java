3e8f0b63cc789c7ecdf4649c3b0710d98ada03c1
mrkirby153 <mr.austinwhyte@gmail.com>
2014-05-17 19:45:28 -0700

    Began command framework

---
+++ src/main/java/me/mrkirby153/plugins/ThePlague/command/BaseCommand.java
@@ -0,0 +1,33 @@
+package me.mrkirby153.plugins.ThePlague.command;
+
+import me.mrkirby153.plugins.ThePlague.ThePlague;
+
+public abstract class BaseCommand {
+    private String permissionRequired = "%all%";
+    private String commandName;
+    private String commandDescription;
+
+    public final String notPlayer = "You must be a player to preform this command!";
+    public ThePlague plugin = ThePlague.instance();
+
+    public BaseCommand() throws Exception {
+    }
+
+    public BaseCommand(String commandName, String commanDescription, String permissionRequired){
+        this.commandName = commandName;
+        this.commandDescription = commanDescription;
+        this.permissionRequired = permissionRequired;
+    }
+
+    public String commandName(){
+        return this.commandName;
+    }
+
+    public String getPermissionRequired(){
+        return this.permissionRequired;
+    }
+
+    public String getCommandDescription(){
+        return this.commandDescription;
+    }
+}
---
+++ src/main/java/me/mrkirby153/plugins/ThePlague/command/Command.java
@@ -0,0 +1,42 @@
+package me.mrkirby153.plugins.ThePlague.command;
+
+import me.mrkirby153.plugins.ThePlague.ThePlague;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Command {
+
+    private static ArrayList<BaseCommand> commands = new ArrayList<BaseCommand>();
+    private static HashMap<String, BaseCommand> aliases = new HashMap<String, BaseCommand>();
+
+    public static void registerComamnd(BaseCommand command){
+        if(!commands.contains(command)){
+            commands.add(command);
+        } else {
+            ThePlague.instance().getLogger().warning("Attempted to register "+command.getName()+" when it is already registered!");
+        }
+    }
+
+    public static BaseCommand findByString(String name){
+        for(BaseCommand cmd : commands){
+            if(cmd.getName().equalsIgnoreCase)(name){
+                    return cmd;
+            }
+        }
+    }
+
+    public static void registerAlias(BaseCommand cmd, String alias){
+        aliases.put(alias, cmd);
+    }
+
+    public static HashMap<String, BaseCommand> getAliases(){
+        return aliases;
+    }
+
+    public static ArrayList<BaseCommand> commandList(){
+        return commands;
+    }
+
+
+}
