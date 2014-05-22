0980bc68c6b1b31e5f5b21cf959b7d5dac5ade06
mrkirby153 <mr.austinwhyte@gmail.com>
2014-05-21 21:17:12 -0700

    Added ability for multiple uninfected spawn locations

--- src/main/java/me/mrkirby153/plugins/ThePlague/arena/Arena.java
+++ src/main/java/me/mrkirby153/plugins/ThePlague/arena/Arena.java
@@ -8,9 +8,12 @@ import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Arena {
     private String name;
@@ -18,6 +21,8 @@ public class Arena {
     private Location l2;
     private World world;
     private ArrayList<Player> playersInGame = new ArrayList<Player>();
+
+    private ArrayList<Location> uninfedtedSpawnLocations = new ArrayList<Location>();
 
     public Arena(String name, Location pt1, Location pt2, World world) {
         this.name = name;
@@ -138,5 +143,40 @@ public class Arena {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    // Save respawn data
+    public void saveRespawnData() {
+        JSONObject spawns = new JSONObject();
+        JSONArray uninfected = new JSONArray();
+        for (Location l : uninfedtedSpawnLocations) {
+            String serialized = l.getWorld().getName() + ":" + l.getBlockX() + ":" + l.getBlockY() + ":" + l.getBlockZ() + ":" +
+                    l.getPitch() + ":" + l.getYaw();
+            uninfected.add(serialized);
+        }
+        spawns.put("uninfected", uninfected);
+        // Write to file
+        try {
+            FileWriter file = new FileWriter(ThePlague.instance().getDataFolder().getAbsolutePath() + File.separator + "data" + File.separator + this.name + File.separator + name + ".spawns");
+            file.write(spawns.toJSONString());
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void addUninfectedSpawn(Location location) {
+        uninfedtedSpawnLocations.add(location);
+        saveRespawnData();
+    }
+
+    public Location removeUninfectedSpawn(int id) {
+        return uninfedtedSpawnLocations.remove(id);
+    }
+
+    public Location randomUninfectedSpawn() {
+        Random r = new Random();
+        return uninfedtedSpawnLocations.get(r.nextInt(uninfedtedSpawnLocations.size()));
     }
 }
