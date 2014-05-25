15ff4f628a5b9b0811d92a1bb3f9c181e9daa2f2
mrkirby153 <mr.austinwhyte@gmail.com>
2014-05-24 19:06:24 -0700

    ArenaUtils now handles saving and loading of arenas

--- src/main/java/me/mrkirby153/plugins/ThePlague/arena/Arena.java
+++ src/main/java/me/mrkirby153/plugins/ThePlague/arena/Arena.java
@@ -1,17 +1,16 @@
 package me.mrkirby153.plugins.ThePlague.arena;
 
 import me.mrkirby153.plugins.ThePlague.ThePlague;
-import me.mrkirby153.plugins.ThePlague.utils.ChatHelper;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -39,110 +38,12 @@ public class Arena {
     }
 
     public void saveArenaToFile() {
-        // Loop through every block and save it to a file
-        ChatHelper.sendAdminMessage("Saving arena " + this.name + " to file. May cause lag!");
-        File arenaDataFolder = new File(ThePlague.instance().getDataFolder().getAbsolutePath() + File.separator + "data" + File.separator + name);
-        if (!arenaDataFolder.exists())
-            arenaDataFolder.mkdirs();
-        File arenaBlocksFile = new File(arenaDataFolder.getAbsolutePath() + File.separator + name + ".arena-blocks");
-        Writer writer;
-        try {
-            writer = new PrintWriter(arenaBlocksFile);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            ChatHelper.sendAdminMessage("Could not open file " + arenaBlocksFile.getAbsolutePath() + " [" + e.getMessage() + "]");
-            return;
-        }
-        float startTime = System.currentTimeMillis();
-        int count = 0;
-        int minX, maxX, minY, maxY, minZ, maxZ;
-        ArrayList<Location> blocks = new ArrayList<Location>();
-        if (l1.getBlockX() < l2.getBlockX()) {
-            minX = l1.getBlockX();
-            maxX = l2.getBlockX();
-        } else {
-            minX = l2.getBlockX();
-            maxX = l1.getBlockX();
-        }
-        if (l1.getBlockY() < l2.getBlockY()) {
-            minY = l1.getBlockY();
-            maxY = l2.getBlockY();
-        } else {
-            minY = l2.getBlockY();
-            maxY = l1.getBlockY();
-        }
-        if (l1.getBlockZ() < l2.getBlockZ()) {
-            minZ = l1.getBlockZ();
-            maxZ = l2.getBlockZ();
-        } else {
-            minZ = l2.getBlockZ();
-            maxZ = l1.getBlockZ();
-        }
-        System.out.println(minZ + "," + maxZ);
-        for (int x = minX; x <= maxX; x++) {
-            for (int y = minY; y <= maxY; y++) {
-                for (int z = minZ; z <= maxZ; z++) {
-                    Location l = new Location(world, x, y, z);
-                    Block b = l.getBlock();
-                    try {
-                        writer.write(String.format("%s,%s,%s,%s:%s,%s", world.getName(), x, y, z, b.getType(), (int) b.getData()) + "\n");
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    count++;
-                }
-            }
-        }
-        try {
-            writer.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        long endTime = System.currentTimeMillis();
-        float totalTime = (endTime - startTime) / 1000;
-        ChatHelper.sendAdminMessage(String.format("Saved %s blocks in %s seconds!", count, totalTime));
-
+        ArenaUtils.saveBlocksToFile(this);
     }
 
     public void loadArenaFromFile() {
         File arenaDataFolder = new File(ThePlague.instance().getDataFolder().getAbsolutePath() + File.separator + "data" + File.separator + name);
-        if (!arenaDataFolder.exists())
-            arenaDataFolder.mkdirs();
-        File arenaBlocksFile = new File(arenaDataFolder.getAbsolutePath() + File.separator + name + ".arena-blocks");
-        ChatHelper.sendAdminMessage("Loading Arena From File...");
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(arenaBlocksFile));
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] parts = line.split(":");
-                if (parts.length != 2)
-                    continue;
-                String[] coords = parts[0].split(",");
-                String[] block = parts[1].split(",");
-                if (coords.length != 4 || block.length != 2)
-                    continue;
-                World w;
-                if (Bukkit.getWorld(coords[0]) != null)
-                    w = Bukkit.getWorld(coords[0]);
-                else
-                    continue;
-                int x = Integer.parseInt(coords[1]);
-                int y = Integer.parseInt(coords[2]);
-                int z = Integer.parseInt(coords[3]);
 
-                Material type = Material.valueOf(block[0]);
-                if (type == null)
-                    continue;
-                Location l = new Location(w, x, y, z);
-                l.getBlock().setType(type);
-                l.getBlock().setData((byte) Integer.parseInt(block[1]));
-            }
-            br.close();
-            ChatHelper.sendAdminMessage("Done!");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     // Save respawn data
