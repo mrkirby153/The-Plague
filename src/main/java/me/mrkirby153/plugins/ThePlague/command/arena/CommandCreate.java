aef7c9a5f07a79392b430aa4e62ffc318508acb7
mrkirby153 <mr.austinwhyte@gmail.com>
2014-05-26 16:17:59 -0700

    More tweaks and changes

--- src/main/java/me/mrkirby153/plugins/ThePlague/command/arena/CommandCreate.java
+++ src/main/java/me/mrkirby153/plugins/ThePlague/command/arena/CommandCreate.java
@@ -1,11 +1,13 @@
 package me.mrkirby153.plugins.ThePlague.command.arena;
 
 import me.mrkirby153.plugins.ThePlague.arena.Arena;
+import me.mrkirby153.plugins.ThePlague.arena.ArenaUtils;
 import me.mrkirby153.plugins.ThePlague.arena.Arenas;
 import me.mrkirby153.plugins.ThePlague.arena.players.ArenaCreator;
 import me.mrkirby153.plugins.ThePlague.command.BaseCommand;
 import me.mrkirby153.plugins.ThePlague.utils.ChatHelper;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -48,40 +50,35 @@ public class CommandCreate extends BaseCommand {
                 test.addUninfectedSpawn(p.getLocation());
                 test.saveRespawnData();
             }
-            if(args[0].equalsIgnoreCase("select")){
-                if (!(sender instanceof Player)) {
-                    sender.sendMessage(notPlayer);
-                    return;
-                }
-                Player p = (Player) sender;
-                ArenaCreator creator = Arenas.findCreatorByName(p.getName());
-                if (creator == null) {
-                    ChatHelper.sendToPlayer(p, ChatColor.RED + "You must be in arena create mode to do that!");
-                    return;
-                }
-                creator.setSelectedArena(null);
-                ChatHelper.sendToPlayer(p, ChatColor.GREEN + "Cleared current selection!");
-            }
         }
         if (args.length == 2) {
-            if (args[0].equalsIgnoreCase("select")) {
+            if (args[0].equalsIgnoreCase("arena")) {
                 if (!(sender instanceof Player)) {
                     sender.sendMessage(notPlayer);
                     return;
                 }
                 Player p = (Player) sender;
-                Arena arena = Arenas.findByName(args[1]);
-                if (arena == null) {
-                    ChatHelper.sendToPlayer(p, ChatColor.RED + "There is no such arena with name '" + args[1] + "'!");
-                    return;
-                }
                 ArenaCreator creator = Arenas.findCreatorByName(p.getName());
                 if (creator == null) {
                     ChatHelper.sendToPlayer(p, ChatColor.RED + "You must be in arena create mode to do that!");
                     return;
                 }
+                String arenaName = args[1];
+                if (Arenas.findByName(arenaName) != null) {
+                    ChatHelper.sendToPlayer(p, ChatColor.RED + "That arena already exists!");
+                    return;
+                }
+                Location pt1 = creator.getPt1();
+                Location pt2 = creator.getPt2();
+                if (pt1 == null || pt2 == null) {
+                    ChatHelper.sendToPlayer(p, ChatColor.RED + "You must select two points first!");
+                    return;
+                }
+                Arena arena = new Arena(arenaName, pt1, pt2, p.getWorld());
+                ArenaUtils.saveBlocksToFile(arena);
+                ArenaUtils.addArena(arena);
                 creator.setSelectedArena(arena);
-                ChatHelper.sendToPlayer(p, ChatColor.GREEN + "Selected arena " + arena.getName() + "!");
+                ChatHelper.sendToPlayer(p, ChatColor.GREEN + "Created and selected arena " + arenaName);
             }
         }
     }
