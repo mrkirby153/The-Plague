63c6d2b6ec967682f2311da50329862961e10819
mrkirby153 <mr.austinwhyte@gmail.com>
2014-05-26 18:49:25 -0700

    Added ability to create lobby

--- src/main/java/me/mrkirby153/plugins/ThePlague/command/arena/CommandCreate.java
+++ src/main/java/me/mrkirby153/plugins/ThePlague/command/arena/CommandCreate.java
@@ -1,8 +1,10 @@
 package me.mrkirby153.plugins.ThePlague.command.arena;
 
 import me.mrkirby153.plugins.ThePlague.arena.Arena;
+import me.mrkirby153.plugins.ThePlague.arena.ArenaNotFoundException;
 import me.mrkirby153.plugins.ThePlague.arena.ArenaUtils;
 import me.mrkirby153.plugins.ThePlague.arena.Arenas;
+import me.mrkirby153.plugins.ThePlague.arena.lobby.Lobby;
 import me.mrkirby153.plugins.ThePlague.arena.players.ArenaCreator;
 import me.mrkirby153.plugins.ThePlague.command.BaseCommand;
 import me.mrkirby153.plugins.ThePlague.utils.ChatHelper;
@@ -27,9 +29,11 @@ public class CommandCreate extends BaseCommand {
             Player p = (Player) sender;
             if (Arenas.findCreatorByName(p.getName()) != null) {
                 Arenas.deactivateCreator(p);
+                Arenas.takeCreationStick(p);
                 ChatHelper.sendToPlayer(p, ChatColor.GREEN + "Deactivated creation mode!");
             } else {
                 Arenas.activateCreator(p);
+                Arenas.giveCreationStick(p);
                 ChatHelper.sendToPlayer(p, ChatColor.GREEN + "Activated creation mode!");
             }
         }
@@ -49,6 +53,64 @@ public class CommandCreate extends BaseCommand {
                 test.saveArenaToFile();
                 test.addUninfectedSpawn(p.getLocation());
                 test.saveRespawnData();
+            }
+            if (args[0].equalsIgnoreCase("lobby")) {
+                if (!(sender instanceof Player)) {
+                    sender.sendMessage(notPlayer);
+                    return;
+                }
+                Player p = (Player) sender;
+                ArenaCreator creator = Arenas.findCreatorByName(p.getName());
+                if (creator == null) {
+                    ChatHelper.sendToPlayer(p, ChatColor.RED + "You must be in arena create mode to do that!");
+                    return;
+                }
+                Arena selectedArena = creator.getSelectedArena();
+                if (selectedArena == null) {
+                    ChatHelper.sendToPlayer(p, ChatColor.RED + "Your selected arena does not exist! Please select another");
+                    return;
+                }
+                Location pt1 = creator.getPt1();
+                Location pt2 = creator.getPt2();
+                if (pt1 == null || pt2 == null) {
+                    ChatHelper.sendToPlayer(p, ChatColor.RED + "You must select two points first!");
+                    return;
+                }
+                try {
+                    Lobby lobby = new Lobby(selectedArena.getName(), pt1, pt2);
+                    Arenas.registerLobby(lobby);
+                    lobby.saveToFile();
+                    ChatHelper.sendToPlayer(p, ChatColor.GREEN + "Created a lobby for arena " + selectedArena.getName());
+                } catch (ArenaNotFoundException e) {
+                    ChatHelper.sendAdminMessage("An error occured when creating a lobby! " + e.getMessage());
+                }
+            }
+            if (args[0].equalsIgnoreCase("lobby-spawn")) {
+                if (!(sender instanceof Player)) {
+                    sender.sendMessage(notPlayer);
+                    return;
+                }
+                Player p = (Player) sender;
+                ArenaCreator creator = Arenas.findCreatorByName(p.getName());
+                if (creator == null) {
+                    ChatHelper.sendToPlayer(p, ChatColor.RED + "You must be in arena create mode to do that!");
+                    return;
+                }
+                Arena selectedArena = creator.getSelectedArena();
+                if (selectedArena == null) {
+                    ChatHelper.sendToPlayer(p, ChatColor.RED + "Your selected arena does not exist! Please select another");
+                    return;
+                }
+                if (Arenas.findLobbyForArena(selectedArena) == null) {
+                    ChatHelper.sendToPlayer(p, ChatColor.RED + "There is no lobby for arena " + selectedArena.getName() + "!");
+                    return;
+                }
+                Location currentLoc = p.getLocation();
+                Lobby lobby = Arenas.findLobbyForArena(selectedArena);
+                lobby.setSpawn(currentLoc);
+                lobby.update();
+                ChatHelper.sendToPlayer(p, ChatColor.GREEN+"Set lobby spawn to your location!");
+
             }
         }
         if (args.length == 2) {
@@ -82,6 +144,7 @@ public class CommandCreate extends BaseCommand {
                 creator.setPt1(null);
                 ChatHelper.sendToPlayer(p, ChatColor.GREEN + "Created and selected arena " + arenaName);
             }
+
         }
     }
 }
