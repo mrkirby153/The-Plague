353c9223d8557fd3d8c61c78dd1b26a8fd014564
mrkirby153 <mr.austinwhyte@gmail.com>
2014-05-26 18:50:35 -0700

    Cancel the even only if the player is not in creation mode

--- src/main/java/me/mrkirby153/plugins/ThePlague/listeners/ArenaListener.java
+++ src/main/java/me/mrkirby153/plugins/ThePlague/listeners/ArenaListener.java
@@ -1,6 +1,7 @@
 package me.mrkirby153.plugins.ThePlague.listeners;
 
 import me.mrkirby153.plugins.ThePlague.arena.ArenaUtils;
+import me.mrkirby153.plugins.ThePlague.arena.Arenas;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -9,7 +10,8 @@ public class ArenaListener implements Listener {
 
     @EventHandler
     public void blockBreak(BlockBreakEvent event) {
-        if (ArenaUtils.isProtected(event.getBlock().getLocation()))
-            event.setCancelled(true);
+        if (ArenaUtils.isProtected(event.getBlock().getLocation())) {
+            event.setCancelled(Arenas.findCreatorByName(event.getPlayer().getName()) == null);
+        }
     }
 }
