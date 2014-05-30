103e97e275a25ff873a5037495ebd14af89da780
mrkirby153 <mr.austinwhyte@gmail.com>
2014-05-30 13:42:10 -0700

    Minor Changes

--- src/main/java/me/mrkirby153/plugins/ThePlague/listeners/ArenaListener.java
+++ src/main/java/me/mrkirby153/plugins/ThePlague/listeners/ArenaListener.java
@@ -4,58 +4,63 @@ package me.mrkirby153.plugins.ThePlague.listeners;
 import me.mrkirby153.plugins.ThePlague.ThePlague;
 import me.mrkirby153.plugins.ThePlague.arena.ArenaUtils;
 import me.mrkirby153.plugins.ThePlague.arena.Arenas;
+import me.mrkirby153.plugins.ThePlague.signs.ArenaSign;
+import me.mrkirby153.plugins.ThePlague.signs.Signs;
+import me.mrkirby153.plugins.ThePlague.utils.ChatHelper;
+import org.bukkit.ChatColor;
 import org.bukkit.Effect;
-import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.ItemFrame;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.player.PlayerBucketEmptyEvent;
 import org.bukkit.event.player.PlayerBucketFillEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class ArenaListener implements Listener {
 
     @EventHandler
-    public void blockBreak(BlockBreakEvent event){
-        if(ArenaUtils.isProtectedLobby(event.getBlock().getLocation()))
+    public void blockBreak(BlockBreakEvent event) {
+        if (ArenaUtils.isProtectedLobby(event.getBlock().getLocation()))
             event.setCancelled(Arenas.findCreatorByName(event.getPlayer().getName()) == null);
     }
 
     @EventHandler
-    public void blockPlace(BlockPlaceEvent event){
-        if(ArenaUtils.isProtectedLobby(event.getBlock().getLocation()))
+    public void blockPlace(BlockPlaceEvent event) {
+        if (ArenaUtils.isProtectedLobby(event.getBlock().getLocation()))
             event.setCancelled(Arenas.findCreatorByName(event.getPlayer().getName()) == null);
     }
 
 
     @EventHandler
-    public void bucketFill(PlayerBucketFillEvent event){
-        if(ArenaUtils.isProtectedLobby(event.getBlockClicked().getLocation())) {
-            event.setCancelled(Arenas.findCreatorByName(event.getPlayer().getName()) == null);
-        }
-    }
-
-    @EventHandler
-    public void bucketEmpty(PlayerBucketEmptyEvent event){
-        if(ArenaUtils.isProtectedLobby(event.getBlockClicked().getLocation())) {
+    public void bucketFill(PlayerBucketFillEvent event) {
+        if (ArenaUtils.isProtectedLobby(event.getBlockClicked().getLocation())) {
             event.setCancelled(Arenas.findCreatorByName(event.getPlayer().getName()) == null);
         }
     }
 
     @EventHandler
-    public void itemFrameInteract(PlayerInteractEntityEvent event){
-        if(event.getRightClicked() instanceof ItemFrame){
-            if(ArenaUtils.isProtected(event.getRightClicked().getLocation())) {
+    public void bucketEmpty(PlayerBucketEmptyEvent event) {
+        if (ArenaUtils.isProtectedLobby(event.getBlockClicked().getLocation())) {
+            event.setCancelled(Arenas.findCreatorByName(event.getPlayer().getName()) == null);
+        }
+    }
+
+    @EventHandler
+    public void itemFrameInteract(PlayerInteractEntityEvent event) {
+        if (event.getRightClicked() instanceof ItemFrame) {
+            if (ArenaUtils.isProtected(event.getRightClicked().getLocation())) {
                 event.setCancelled(Arenas.findCreatorByName(event.getPlayer().getName()) == null);
             }
         }
     }
-
     @EventHandler
     public void onSignClick(PlayerInteractEvent event){
         if(event.getAction().equals(Action.RIGHT_CLICK_BLOCK) || event.getAction().equals(Action.RIGHT_CLICK_AIR)){
